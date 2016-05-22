61c6166f833dee6621dc8c96fedac0f03ab4347a
wantongtang <wantongtang@foxmail.com>
2016-05-22 23:20:06 +0800

    shodan更加自动化

--- myshodan.py
+++ myshodan.py
@@ -1,6 +1,10 @@
 import shodan
 mykey = '5ISgqbZ9usGsN9MP1yv5LiYTzrj4HhKm'
 api=shodan.Shodan(mykey)
-results=api.search('apache')
+results=api.search('port:2375 X-Content-Type-Options: nosniff')
+
+output=open('shodan.txt','a')
 for result in results['matches']:
 	print 'IP:%s'%result['ip_str']
+        output.write("\n%s"%(result['ip_str']))
+output.close()
